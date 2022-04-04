44be3a2eb1e58378f18ace1ce9cb965dcb95d2c4
Mateusz Hamera <mateuszhamera2@gmail.com>
2022-04-04 23:18:56 +0200

    delete files

--- src/PrintFromFile.cpp
+++
@@ -1,6 +0,0 @@
-#include "../header/PrintFromFile.h"
-
-void PrintFromFile::printFromFile(Read* objectToPrint,Flag::Value flags, uint32_t numOfLine)
-{
-    std::cout<<objectToPrint->getStringFromFile(flags,numOfLine)<<std::endl;
-}
