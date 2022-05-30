82a8895cdd848c9f495f4053e28d899469257321
Mateusz Hamera <mateuszhamera2@gmail.com>
2022-05-30 14:20:03 +0200

    reformat code

--- exceptions/FailOpen.h
+++ exceptions/FailOpen.h
@@ -1,16 +1,18 @@
 #include <exception>
 #include <iostream>
+
 #ifndef FAIL_OPEN_H
 #define FAIL_OPEN_H
-class FailOpen : public std::exception
-{
+
+class FailOpen : public std::exception {
 private:
     std::string _message;
 public:
-    FailOpen(std::string message) : _message(message){}
-    virtual const char* what() const noexcept override
-    {
+    FailOpen(std::string message) : _message(message) {}
+
+    virtual const char *what() const noexcept override {
         return _message.c_str();
     }
 };
+
 #endif
--- exceptions/InvalidInput.h
+++ exceptions/InvalidInput.h
@@ -1,16 +1,18 @@
 #include <exception>
 #include <iostream>
+
 #ifndef INVALID_INPUT_H
 #define INVALID_INPUT_H
-class InvalidInput : public std::exception
-{
+
+class InvalidInput : public std::exception {
 private:
     std::string _message;
 public:
-    InvalidInput(std::string message) : _message(message){}
-    virtual const char* what() const noexcept override
-    {
+    InvalidInput(std::string message) : _message(message) {}
+
+    virtual const char *what() const noexcept override {
         return _message.c_str();
     }
 };
+
 #endif
