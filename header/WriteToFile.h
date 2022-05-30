94ab03207e7d62232abbdecd0267575c15f28080
Mateusz Hamera <mateuszhamera2@gmail.com>
2022-05-30 14:27:43 +0200

    Write to file class

--- header/WriteToFile.h
+++ header/WriteToFile.h
@@ -4,12 +4,20 @@
 
 #ifndef PIZZATIME_WRITETOFILE_H
 #define PIZZATIME_WRITETOFILE_H
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 #include "../exceptions/FailOpen.h"
 #include <iostream>
 
 class WriteToFile {
 public:
+<<<<<<< Updated upstream
     static void saveToFile(std::string path,std::string content);
+=======
+    static void saveToFile(std::string path, std::string content);
+>>>>>>> Stashed changes
 };
 
 
--- src/WriteToFile.cpp
+++ src/WriteToFile.cpp
@@ -7,9 +7,16 @@
 
 void WriteToFile::saveToFile(std::string path, std::string content) {
     std::ofstream file(path);
+<<<<<<< Updated upstream
     if (!file.is_open()){
         throw FailOpen("Cannot open file: " + path + "\n");
     }
     file<<content;
+=======
+    if (!file.is_open()) {
+        throw FailOpen("Cannot open file: " + path + "\n");
+    }
+    file << content;
+>>>>>>> Stashed changes
     file.close();
 }
