0ef1afb36cfa8e2982a4300c3b959060b6d24b39
Mateusz Hamera <mateuszhamera2@gmail.com>
2022-05-30 15:47:20 +0200

    refracot code WriteToFile

--- header/WriteToFile.h
+++ header/WriteToFile.h
@@ -4,28 +4,13 @@
 
 #ifndef PIZZATIME_WRITETOFILE_H
 #define PIZZATIME_WRITETOFILE_H
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
-=======
-
->>>>>>> Stashed changes
 #include "../exceptions/FailOpen.h"
 #include <iostream>
 
 class WriteToFile {
 public:
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    static void saveToFile(std::string path,std::string content);
-=======
     static void saveToFile(std::string path, std::string content);
->>>>>>> Stashed changes
-=======
-    static void saveToFile(std::string path, std::string content);
->>>>>>> Stashed changes
 };
 
 
