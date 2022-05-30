733e41eefb0f67fb962769072911a221248e08e5
Mateusz Hamera <mateuszhamera2@gmail.com>
2022-05-30 14:42:30 +0200

    refractor code

--- header/MenuPos.h
+++ header/MenuPos.h
@@ -1,31 +1,60 @@
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+#ifndef MENU_POS_H
+#define MENU_POS_H
+=======
+>>>>>>> Stashed changes
 //
 // Created by mateusz on 29.05.22.
 //
 
 #ifndef PIZZATIME_MENUPOS_H
 #define PIZZATIME_MENUPOS_H
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 #include <iostream>
 #include <vector>
+#include "ConvertString.h"
+
 #define SEPARATOR "\t"
+<<<<<<< Updated upstream
 #define PRICE_SEPARATOR "\n\t\t\t\t"
+=======
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
 class MenuPos
 {
+=======
+#define PRICE_SEPARATOR "\n\t\t\t\t"
+
+class MenuPos {
+>>>>>>> Stashed changes
 private:
     uint32_t id;
     std::string name;
     double smallSize;
     double mediumSize;
     double largeSize;
-    std::vector<std::string> splitStr(std::string input);
 
 public:
     MenuPos(std::string inputStringFromFile);
+
     void setValuesFromString(std::string inputStringFromFile);
+
     uint32_t getId();
+
     std::string getName();
+
     double getSmallSize();
+
     double getMediumSize();
+
     double getLargeSize();
+
     friend std::ostream &operator<<(std::ostream &out, const MenuPos toPrint);
 };
 
--- src/MenuPos.cpp
+++ src/MenuPos.cpp
@@ -1,13 +1,11 @@
 #include "../header/MenuPos.h"
 
-MenuPos::MenuPos(std::string inputStringFromFile)
-{
+MenuPos::MenuPos(std::string inputStringFromFile) {
     setValuesFromString(inputStringFromFile);
 }
 
-void MenuPos::setValuesFromString(std::string inputStringFromFile)
-{
-    std::vector<std::string> tmp = splitStr(inputStringFromFile);
+void MenuPos::setValuesFromString(std::string inputStringFromFile) {
+    std::vector<std::string> tmp = ConvertString::splitStr(inputStringFromFile, ',');
     id = std::stoi(tmp[0]);
     name = tmp[1];
     smallSize = std::stod(tmp[2]);
@@ -15,53 +13,35 @@ void MenuPos::setValuesFromString(std::string inputStringFromFile)
     largeSize = std::stod(tmp[4]);
 }
 
-std::vector<std::string> MenuPos::splitStr(std::string input)
-{
-    std::vector<std::string> inputSeparate;
-    std::string tmp = "";
-    
-    for (auto &&i : input)
-    {
-        if (i == ',')
-        {
-            inputSeparate.push_back(tmp);
-            tmp = "";
-        }
-        else
-        {
-           tmp += i;
-        }
-    }
-    inputSeparate.push_back(tmp);
-    return inputSeparate;
-}
-
-uint32_t MenuPos::getId()
-{
+uint32_t MenuPos::getId() {
     return id;
 }
 
-std::string MenuPos::getName()
-{
+std::string MenuPos::getName() {
     return name;
 }
 
-double MenuPos::getSmallSize()
-{
+double MenuPos::getSmallSize() {
     return smallSize;
 }
-double MenuPos::getMediumSize()
-{
+
+double MenuPos::getMediumSize() {
     return mediumSize;
 }
-double MenuPos::getLargeSize()
-{
+
+double MenuPos::getLargeSize() {
     return largeSize;
 }
 
+<<<<<<< Updated upstream
 std::ostream& operator<<(std::ostream& out,const MenuPos toPrint)
 {
     out<<toPrint.id<<"."<<toPrint.name<<SEPARATOR<<PRICE_SEPARATOR <<toPrint.smallSize<<SEPARATOR 
         <<toPrint.mediumSize<<SEPARATOR <<toPrint.largeSize;
+=======
+std::ostream &operator<<(std::ostream &out, const MenuPos toPrint) {
+    out << toPrint.id << "." << toPrint.name << SEPARATOR << PRICE_SEPARATOR << toPrint.smallSize << SEPARATOR
+        << toPrint.mediumSize << SEPARATOR << toPrint.largeSize;
+>>>>>>> Stashed changes
     return out;
 }
